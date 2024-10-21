6c39286218f8905cfed55cc05be2d1d49b51d019
Lmdingi <lmdingi4@gmail.com>
2024-10-21 20:01:19 +0200

    Add project files.

---
+++ DataAccess/DBContext/TaskmatesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.DBContext
+{
+    public class TaskmatesDbContext : DbContext
+    {
+        public TaskmatesDbContext()
+        {
+        }
+
+        public TaskmatesDbContext(DbContextOptions<TaskmatesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DemoEmailList> DemoEmailLists { get; set; }
+    }
+}
---
+++ Web.API/Controllers/DemoEmailController.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.DBContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoEmailController : ControllerBase
+    {
+        private readonly TaskmatesDbContext _dbContext;
+
+        public DemoEmailController(TaskmatesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var list = await _dbContext.DemoEmailLists.ToListAsync();
+
+                if (list.Count > 0)
+                {
+                    return Ok(list);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
